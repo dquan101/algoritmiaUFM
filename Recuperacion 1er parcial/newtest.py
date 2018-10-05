42d8122a1a94bde16cf0d662ba40d5f7f8a890ee
dquan101 <41695356+dquan101@users.noreply.github.com>
2018-10-04 18:21:58 -0600

    Se agrego el archivo de python comentado y un README

--- Recuperacion 1er parcial/newtest.py
+++ Recuperacion 1er parcial/newtest.py
@@ -4,14 +4,14 @@ import random
 import sys
 import numpy as np
 
-def readfile(fname):
+def readfile(fname): # Abrir y leer el archivo
     with open(fname) as f:
         contenido = f.readlines()
 
     contenido = [x.strip() for x in contenido]
     return contenido
 
-def writefile(fname):
+def writefile(fname): # Función para escribir sobre el archivo
     with open("write.txt", "a") as f:
         for x in range(len(fname)):
             f.write(fname[x]+"\n")
@@ -29,7 +29,7 @@ def charrd(file):
         array.append(''.join(chr(i) for i in file[x]))
     return array
 
-def merge(*args):
+def merge(*args): # Comparación y unión de arrays para el merge sort
     left, right = args[0] if len(args) == 1 else args
     left_length, right_length = len(left), len(right)
     left_index, right_index = 0, 0
@@ -48,7 +48,7 @@ def merge(*args):
     return merged
 
 
-def merge_sort(data):
+def merge_sort(data): # Checkea si el array es de tamaño uno, y separa los arrays para el merge sort
     length = len(data)
     if length <= 1:
         return data
@@ -58,7 +58,7 @@ def merge_sort(data):
     return merge(left, right)
 
 
-def merge_sort_parallel(data):
+def merge_sort_parallel(data): # Crea la piscina de trabajadores para el procesamiento en paraleloS
     processes = multiprocessing.cpu_count()
     pool = multiprocessing.Pool(processes=processes)
     size = int(math.ceil(float(len(data)) / processes))
@@ -80,4 +80,4 @@ if __name__ == "__main__":
     for sort in merge_sort, merge_sort_parallel:
         data_sorted = sort(data_ascii)
     writefile(charrd(data_sorted))
-    print(charrd(data_sorted))
+    print("Las oraciones ordenadas se encuentran en: write.txt")
